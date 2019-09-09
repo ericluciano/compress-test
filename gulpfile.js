82149e87c282bfc27997f8eba795f7f6887c71b8
ericluciano <ericluciano.rj@gmail.com>
2019-09-09 16:14:28 -0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,20 @@ const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 
-gulp.task('default', () => {
+gulp.task('minify.image', () => {
     return gulp.src('../images/**/*.jpg')
         .pipe(imagemin([
             imageminMozjpeg({
-                quality: 50
+                quality: 93
             }),
             imagemin.optipng({optimizationLevel: 5})
         ]))
         .pipe(gulp.dest('../build/images/'))
 })
+
+gulp.task('copy.xml', function () {
+    gulp.src('../images/**/*.xml')
+        .pipe(gulp.dest('../build/images/'))
+})
+
+gulp.task('execute', ['minify.image', 'copy.xml'])
